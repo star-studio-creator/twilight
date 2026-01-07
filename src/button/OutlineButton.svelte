ed73fec15cbe442c799af7dfa306c736d77973de
FHU-yezi <yehaowei20060411@qq.com>
2026-01-07 21:46:27 +0800

    feat: 为 OutlineButton 文字添加颜色

--- src/button/OutlineButton.svelte
+++ src/button/OutlineButton.svelte
@@ -19,16 +19,16 @@
 <BaseButton className={
   clsx(
     className,
-    "flex gap-1.5 items-center border rounded-md shadow px-2 py-1.5 transition-all dark:text-gray-50",
+    "flex gap-1.5 items-center border rounded-md shadow px-2 py-1.5 transition-all",
     {
       "active:scale-95 hover:opacity-90 hover:shadow-md": !disabled,
       "opacity-70": disabled,
     },
     {
-      "border-gray-950 shadow-gray-950/30 dark:text-gray-50 dark:border-gray-50 dark:shadow-gray-50/30": type ==="default",
-      "border-blue-600 shadow-blue-600/30 dark:border-blue-700 dark:shadow-blue-700/30": type === "primary",
-      "border-orange-500 shadow-orange-500/30 dark:border-orange-600 dark:shadow-orange-600/30": type ==="warning",
-      "border-red-600 shadow-red-600/30 dark:border-red-700 dark:shadow-red-700/30": type === "danger"
+      "text-gray-950 border-gray-950 shadow-gray-950/30 dark:text-gray-50 dark:border-gray-50 dark:shadow-gray-50/30": type ==="default",
+      "text-blue-600 border-blue-600 shadow-blue-600/30 dark:text-blue-700 dark:border-blue-700 dark:shadow-blue-700/30": type === "primary",
+      "text-orange-500 border-orange-500 shadow-orange-500/30 dark:text-orange-600 dark:border-orange-600 dark:shadow-orange-600/30": type ==="warning",
+      "text-red-600 border-red-600 shadow-red-600/30 dark:text-red-700 dark:border-red-700 dark:shadow-red-700/30": type === "danger"
     },
     !disabled && {
       "hover:bg-gray-950/5 dark:hover:bg-gray-50/5": type ==="default",
@@ -39,13 +39,13 @@
   )
 } {onClick} {disabled} {...props}>
     {@const Icon = icon}
-  
+
     {#if iconPosition === "left"}
         <Icon size={18} />
     {/if}
-  
+
     {@render children()}
-  
+
     {#if iconPosition === "right"}
         <Icon size={18} />
     {/if}
