7dc5f40aa0f5c21602e2dfa0c0e63f8574a5c513
FHU-yezi <yehaowei20060411@qq.com>
2026-01-08 21:15:58 +0800

    chore: 添加基础打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
+  plugins: [tailwindcss(), svelte(), dts()],
+  build: {
+    lib: {
+      entry: {
+        button: "./src/button/index.ts",
+        switch: "./src/switch/index.ts",
+      },
+      formats: ["es"],
+    },
+  },
 });
--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), svelte()],
   root: "demo",
+  plugins: [tailwindcss(), svelte()],
 });
