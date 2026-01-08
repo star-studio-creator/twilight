c0d2c862cf0073856f79b3a90903fd6555c7cf5b
FHU-yezi <yehaowei20060411@qq.com>
2026-01-08 21:35:05 +0800

    chore: 排除 `demo/` 目录和 `peerDependencies`

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), svelte(), dts()],
+  plugins: [
+    tailwindcss(),
+    svelte(),
+    dts({
+      include: "src/**/*",
+    }),
+  ],
   build: {
     lib: {
       entry: {
@@ -12,6 +18,13 @@ export default defineConfig({
         switch: "./src/switch/index.ts",
       },
       formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["@lucide/svelte", "clsx", "svelte", "tailwindcss"],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: "src",
+      },
     },
   },
 });
