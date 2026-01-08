21645c3168355f943c0d777804e362ada03592ba
FHU-yezi <yehaowei20060411@qq.com>
2026-01-09 01:51:55 +0800

    chore: 使用 `svelte-package` 正确实现打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,7 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
-import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    svelte(),
-    dts({
-      include: "src/**/*",
-    }),
-  ],
-  build: {
-    lib: {
-      entry: {
-        button: "./src/button.ts",
-        switch: "./src/switch.ts",
-      },
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: (id) => {
-        // 外部化 peerDependencies
-        return (
-          id === "@lucide/svelte" ||
-          id === "clsx" ||
-          id === "svelte" ||
-          id === "tailwindcss" ||
-          // 防止打包 Svelte 内部模块
-          id.includes("svelte/src/internal")
-        );
-      },
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: "src",
-        paths: (id) => {
-          if (!id.includes("svelte/src/internal")) {
-            return id;
-          }
-
-          // 处理 Svelte 内部模块的导入，将其指向外部安装的 Svelte 库
-          const path = id.split("/svelte/src/internal/")[1];
-          return `svelte/src/internal/${path}`;
-        },
-      },
-    },
-  },
+  plugins: [tailwindcss(), svelte()],
 });
