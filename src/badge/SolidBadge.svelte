13b22f793b1882ecd1839f0f043548bcdfda115f
叶子 <yehaowei20060411@qq.com>
2026-01-11 01:37:17 +0800

    fix: 使用正确的 Tailwind CSS 灰色 (#30)
    Close #26

--- src/badge/SolidBadge.svelte
+++ src/badge/SolidBadge.svelte
@@ -13,9 +13,9 @@
 <span
     class={[
         className,
-        "flex items-center gap-1 px-2 rounded-full text-gray-50",
+        "flex items-center gap-1 px-2 rounded-full text-neutral-50",
         {
-            "bg-gray-950 dark:text-gray-950 dark:bg-gray-50":
+            "bg-neutral-950 dark:text-neutral-950 dark:bg-neutral-50":
                 color === "default",
             "bg-green-600 dark:bg-green-700": color === "success",
             "bg-blue-600 dark:bg-blue-700": color === "primary",
