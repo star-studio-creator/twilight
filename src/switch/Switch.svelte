539cf07868cb204407179630b532c267a144a04f
叶子 <yehaowei20060411@qq.com>
2026-01-11 14:36:46 +0800

    feat: 使用 `<input type="checkbox" role="switch" />` 实现 Switch 组件 (#33)
    Close #32

--- src/switch/Switch.svelte
+++ src/switch/Switch.svelte
@@ -17,39 +17,37 @@
     }: Props = $props();
 </script>
 
-<!-- svelte-ignore a11y_consider_explicit_label: 使用时外层会包裹 label 元素 -->
-<button
-    type="button"
-    class={[
-        "w-10 h-6 pl-0.5 rounded-full shadow transition-all",
-        {
-            "cursor-pointer active:scale-95 hover:opacity-90 hover:shadow-md":
-                !disabled,
-            "cursor-not-allowed opacity-50": disabled,
-        },
-        !selected &&
-            "bg-neutral-300 dark:bg-neutral-700 shadow-neutral-300/30 dark:shadow-neutral-700/30",
-        selected && {
-            "bg-neutral-950 dark:bg-neutral-500 shadow-neutral-950/30 dark:shadow-neutral-500/30":
-                color === "default",
-            "bg-blue-600 dark:bg-blue-700 shadow-blue-600/30 dark:shadow-blue-700/30":
-                color === "primary",
-            "bg-orange-500 dark:bg-orange-600 shadow-orange-500/30 dark:shadow-orange-600/30":
-                color === "warning",
-            "bg-red-600 dark:bg-red-700 shadow-red-600/30 dark:shadow-red-700/30":
-                color === "danger",
-        },
-    ]}
-    onclick={() => {
-        selected = !selected;
-    }}
-    role="switch"
-    aria-checked={selected}
-    {disabled}
->
+<div class="flex items-center relative">
+    <input
+        type="checkbox"
+        role="switch"
+        class={[
+            "appearance-none w-10 h-6 rounded-full transition-all",
+            {
+                "cursor-pointer active:scale-95 hover:opacity-90 hover:shadow-md":
+                    !disabled,
+                "cursor-not-allowed opacity-50": disabled,
+            },
+            !selected &&
+                "bg-neutral-300 dark:bg-neutral-700 shadow-neutral-300/30 dark:shadow-neutral-700/30",
+            selected && {
+                "bg-neutral-950 dark:bg-neutral-500 shadow-neutral-950/30 dark:shadow-neutral-500/30":
+                    color === "default",
+                "bg-blue-600 dark:bg-blue-700 shadow-blue-600/30 dark:shadow-blue-700/30":
+                    color === "primary",
+                "bg-orange-500 dark:bg-orange-600 shadow-orange-500/30 dark:shadow-orange-600/30":
+                    color === "warning",
+                "bg-red-600 dark:bg-red-700 shadow-red-600/30 dark:shadow-red-700/30":
+                    color === "danger",
+            },
+        ]}
+        bind:checked={selected}
+        {disabled}
+    />
+
     <div
         class={[
-            "grid place-content-center bg-neutral-50 text-neutral-950 size-5 rounded-full transition-transform pointer-events-none",
+            "absolute ml-0.5 grid place-content-center bg-neutral-50 text-neutral-950 size-5 rounded-full transition-transform pointer-events-none",
             selected && "translate-x-4",
         ]}
     >
@@ -58,4 +56,4 @@
             <Icon size={14} />
         {/if}
     </div>
-</button>
+</div>
