4dee1b41eb6d7464fd3d5950b38fc8e5b906f6de
FHU-yezi <yehaowei20060411@qq.com>
2026-03-19 13:55:06 +0800

    fix: 修复 Lint 报错

--- src/card/Card.svelte
+++ src/card/Card.svelte
@@ -24,13 +24,13 @@ const {
     "border rounded-md shadow-md",
     {
       "p-2": size === "sm",
-      "p-4": size=== "md",
+      "p-4": size === "md",
       "p-6": size === "lg",
     },
     {
-      "border-blue-600 shadow-blue-600/10": color == "primary",
-      "border-neutral-600 shadow-neutral-600/10": color == "secondary",
-      "border-red-600 shadow-red-600/10": color == "danger",
+      "border-blue-600 shadow-blue-600/10": color === "primary",
+      "border-neutral-600 shadow-neutral-600/10": color === "secondary",
+      "border-red-600 shadow-red-600/10": color === "danger",
     },
   ]}
 >
