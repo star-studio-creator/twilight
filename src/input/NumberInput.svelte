9594a34b99e8de7b0fa4d146ceaab8cf1375d01b
FHU-yezi <yehaowei20060411@qq.com>
2026-03-23 10:54:17 +0800

    feat: 优化 Select 组件样式

--- src/input/NumberInput.svelte
+++ src/input/NumberInput.svelte
@@ -12,17 +12,17 @@
 
 <input
   class={[
-    className,
     {
       "text-sm px-2 py-0.5": size === "sm",
       "px-2 py-0.5": size === "md",
       "text-lg px-3 py-1": size === "lg",
     },
-    "border rounded-md shadow border-neutral-300 shadow-neutral-300/20 transition-shadow",
     {
       "hover:shadow-md": !disabled,
       "cursor-not-allowed opacity-70": disabled,
     },
+    "border rounded-md shadow border-neutral-300 shadow-neutral-300/20 transition-shadow",
+    className,
   ]}
   type="number"
   bind:value
