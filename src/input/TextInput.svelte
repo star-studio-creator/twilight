6e78847178e99aa36b932d4a1b732d846332e524
FHU-yezi <yehaowei20060411@qq.com>
2026-03-23 10:43:05 +0800

    feat: 优化 Input 组件样式

--- src/input/TextInput.svelte
+++ src/input/TextInput.svelte
@@ -13,15 +13,18 @@
 
 <input
   class={[
-        className,
-        {
-          "text-sm px-2 py-0.5": size === "sm",
-          "px-2 py-0.5": size === "md",
-          "text-lg px-3 py-1": size === "lg",
-        },
-        "px-2 py-0.5 border rounded-md shadow-md border-neutral-300 shadow-neutral-300/30",
-        disabled && "cursor-not-allowed opacity-70"
-    ]}
+    className,
+    {
+      "text-sm px-2 py-0.5": size === "sm",
+      "px-2 py-0.5": size === "md",
+      "text-lg px-3 py-1": size === "lg",
+    },
+    "border rounded-md shadow border-neutral-300 shadow-neutral-300/20 transition-shadow",
+    {
+      "hover:shadow-md": !disabled,
+      "cursor-not-allowed opacity-70": disabled,
+    },
+  ]}
   type={shadowed ? "password" : "text"}
   bind:value
   {disabled}
