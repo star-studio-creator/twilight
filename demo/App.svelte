383679f5308eadc146803c4985bd4f134571d18b
FHU-yezi <yehaowei20060411@qq.com>
2026-03-24 11:36:53 +0800

    feat: 演示网页添加暗色主题切换

--- demo/App.svelte
+++ demo/App.svelte
@@ -3,18 +3,33 @@
   import ChevronLeft from "@lucide/svelte/icons/chevron-left";
   import { Router } from "sv-router";
   import { GhostButton } from "@/button";
+  import Switch from "@/switch/Switch.svelte";
+  import { getTheme, setTheme } from "@/theme";
+
+  let isDarkTheme = $state(getTheme() === "dark");
+
+  $effect(() => {
+    setTheme(isDarkTheme ? "dark" : "light");
+  });
 </script>
 
 <div
-  class="flex flex-col gap-4 text-neutral-950 dark:text-neutral-50 max-w-[60vw] mx-auto my-8"
+  class="relative flex flex-col gap-4 text-neutral-950 dark:text-neutral-50 max-w-[60vw] mx-auto my-8"
 >
-  {#if isActive("/")}
-    <h1 class="text-center text-2xl font-bold">暮光 / Twilight</h1>
-  {:else}
-    <GhostButton onClick={() => navigate("/")} icon={ChevronLeft}>
-      Back
-    </GhostButton>
-  {/if}
+  <div class="flex justify-between items-center">
+    {#if isActive("/")}
+      <h1 class="text-center text-2xl font-bold">暮光 / Twilight</h1>
+    {:else}
+      <GhostButton onClick={() => navigate("/")} icon={ChevronLeft}>
+        Back
+      </GhostButton>
+    {/if}
+
+    <label class="flex gap-2">
+      暗色主题
+      <Switch bind:enabled={isDarkTheme} />
+    </label>
+  </div>
 
   <Router />
 </div>
