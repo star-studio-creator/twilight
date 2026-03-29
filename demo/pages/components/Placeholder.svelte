8bc78a630d1dd777233899422e498e1a361abecb
FHU-yezi <yehaowei20060411@qq.com>
2026-03-29 16:18:03 +0800

    feat: 添加 Placeholder 组件

---
+++ demo/pages/components/Placeholder.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { COLORS } from "@demo/constants";
+  import { Placeholder } from "@/placeholder";
+</script>
+
+{#each COLORS as color}
+  <Placeholder class="w-full h-32" {color} />
+{/each}
