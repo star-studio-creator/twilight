eb4e24dd1e064b855240a045291bd656fb9cfa88
FHU-yezi <yehaowei20060411@qq.com>
2026-03-29 12:30:22 +0800

    feat: 添加内容区样式

--- src/disclosure/Disclosure.svelte
+++ src/disclosure/Disclosure.svelte
@@ -25,52 +25,69 @@
   let open = $state(false);
 </script>
 
-<button
-  type="button"
-  class={[
-    {
-      "text-sm px-3 py-1": size === "sm",
-      "px-4 py-2": size === "md",
-      "text-lg px-4 py-3": size === "lg",
-    },
-    {
-      "text-blue-600 dark:text-blue-400": color === "primary",
-      "text-neutral-950 dark:text-neutral-300": color === "secondary",
-      "text-red-600 dark:text-red-500": color === "danger",
-    },
-    "w-full font-bold border rounded-md shadow hover:shadow-md cursor-pointer",
-    "hover:bg-current/5 dark:hover:bg-current/10 shadow-current/5 dark:shadow-current/10 transition-all"
-  ]}
-  onclick={() => open = !open}
->
-  <div class="flex justify-between items-center">
-    <div class="flex items-center gap-2">
-      {#if icon}
-        {@const Icon = icon}
-
-        <Icon size="1em" />
-      {/if}
-
-      <p>{title}</p>
-    </div>
-
-    <ChevronDown
-      class={[
-      open === true && "rotate-180",
-      "transition-transform"
-    ]}
-      size="1em"
-      strokeWidth={3}
-    />
-  </div>
-</button>
-
-{#if open}
-  <div
+<div>
+  <button
+    type="button"
     class={[
-      className
+      {
+        "text-sm px-3 py-1": size === "sm",
+        "px-4 py-2": size === "md",
+        "text-lg px-4 py-3": size === "lg",
+      },
+      {
+        "text-blue-600 dark:text-blue-400": color === "primary",
+        "text-neutral-950 dark:text-neutral-300": color === "secondary",
+        "text-red-600 dark:text-red-500": color === "danger",
+      },
+      {
+        "border-x border-t rounded-t-md": open,
+        "border rounded-md": !open,
+      },
+      "w-full font-bold shadow hover:shadow-md cursor-pointer",
+      "hover:bg-current/5 dark:hover:bg-current/10 shadow-current/5 dark:shadow-current/10 transition-colors"
     ]}
+    onclick={() => open = !open}
   >
-    {@render children()}
-  </div>
-{/if}
+    <div class="flex justify-between items-center">
+      <div class="flex items-center gap-2">
+        {#if icon}
+          {@const Icon = icon}
+
+          <Icon size="1em" />
+        {/if}
+
+        <p>{title}</p>
+      </div>
+
+      <ChevronDown
+        class={[
+          open === true && "rotate-180",
+          "transition-transform"
+        ]}
+        size="1em"
+        strokeWidth={3}
+      />
+    </div>
+  </button>
+
+  {#if open}
+    <div
+      class={[
+        {
+          "text-sm px-3 py-1": size === "sm",
+          "px-4 py-2": size === "md",
+          "text-lg px-4 py-3": size === "lg",
+        },
+        {
+          "border-blue-600 dark:border-blue-400": color === "primary",
+          "border-neutral-950 dark:border-neutral-300": color === "secondary",
+          "border-red-600 dark:border-red-500": color === "danger",
+        },
+        "rounded-b-md border",
+        className
+      ]}
+    >
+      {@render children()}
+    </div>
+  {/if}
+</div>
