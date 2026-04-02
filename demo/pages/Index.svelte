76f91deb8a2c80b0bac9fe15cbb37c6f54f27f5b
FHU-yezi <yehaowei20060411@qq.com>
2026-04-03 02:23:38 +0800

    feat: 移除冗余的 size !important

--- demo/pages/Index.svelte
+++ demo/pages/Index.svelte
@@ -50,10 +50,10 @@
     {@const Icon = route.icon}
 
     <a
-      class="flex flex-col gap-2 p-4 border border-neutral-600 rounded-md shadow-md shadow-neutral-600/10"
+      class="flex flex-col gap-2 p-4 text-2xl border border-neutral-600 rounded-md shadow-md shadow-neutral-600/10"
       href={route.path}
     >
-      <Icon class="size-6!" />
+      <Icon />
       <h2 class="text-lg font-bold">{route.name}</h2>
     </a>
   {/each}
