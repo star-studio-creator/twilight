cc341f26acb66b8fc9adb2fb541de31bcd11c362
FHU-yezi <yehaowei20060411@qq.com>
2026-04-02 16:38:16 +0800

    feat: 优化 Toast 样式

--- src/toast/Toaster.svelte
+++ src/toast/Toaster.svelte
@@ -3,42 +3,36 @@
   import CircleCheck from "@lucide/svelte/icons/circle-check";
   import CircleX from "@lucide/svelte/icons/circle-x";
   import Info from "@lucide/svelte/icons/info";
-  import LoaderCircle from "@lucide/svelte/icons/loader-circle";
-  import { onMount } from "svelte";
   import { Toaster } from "svelte-sonner";
-  import { getTheme, type ThemeType } from "@/theme";
-
-  // 将 Tailwind CSS Dark Mode 与 Svelte Sonner Theme 同步
-  let theme: ThemeType = $state(getTheme());
-
-  onMount(() => {
-    const observer = new MutationObserver(() => {
-      theme = getTheme();
-    });
-
-    observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ["class"],
-    });
-
-    return () => observer.disconnect();
-  });
 </script>
 
-<Toaster position="top-right" richColors {theme}>
+<Toaster
+  position="top-right"
+  expand
+  toastOptions={{
+    unstyled: true,
+    classes: {
+      toast: "min-[600px]:min-w-70 overflow-hidden flex gap-1.5 px-3 py-2 border rounded-md shadow-md shadow-current/10",
+      icon: "mt-1",
+      title: "font-bold",
+      description: "text-sm",
+      success: "text-green-600 bg-green-50 dark:text-green-400 dark:bg-green-950",
+      info: "text-blue-600 bg-blue-50 dark:text-blue-400 dark:bg-blue-950",
+      warning: "text-orange-600 bg-orange-50 dark:text-orange-500 dark:bg-orange-950",
+      error: "text-red-600 bg-red-50 dark:text-red-500 dark:bg-red-950",
+    }
+  }}
+>
   {#snippet successIcon()}
-    <CircleCheck class="size-5!" />
+    <CircleCheck class="size-4!" />
   {/snippet}
   {#snippet infoIcon()}
-    <Info class="size-5!" />
+    <Info class="size-4!" />
   {/snippet}
   {#snippet warningIcon()}
-    <CircleAlert class="size-5!" />
+    <CircleAlert class="size-4!" />
   {/snippet}
   {#snippet errorIcon()}
-    <CircleX class="size-5!" />
-  {/snippet}
-  {#snippet loadingIcon()}
-    <LoaderCircle class="size-5!" />
+    <CircleX class="size-4!" />
   {/snippet}
 </Toaster>
