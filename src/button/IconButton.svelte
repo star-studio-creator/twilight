d16f70dec44b94e70f25c9bb36b82c27d71a3055
FHU-yezi <yehaowei20060411@qq.com>
2026-04-02 20:25:42 +0800

    feat: 添加 IconButton 组件

---
+++ src/button/IconButton.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import type { LucideIcon } from "@lucide/svelte";
+  import LoaderCircle from "@lucide/svelte/icons/loader-circle";
+  import type { ClassValue, HTMLButtonAttributes } from "svelte/elements";
+  import type { ColorType, SizeType } from "../types";
+  import BaseButton from "./BaseButton.svelte";
+
+  interface Props extends HTMLButtonAttributes {
+    class?: ClassValue;
+    size?: SizeType;
+    color?: ColorType;
+    label: string;
+    onclick?: () => void;
+    loading?: boolean;
+    disabled?: boolean;
+    icon: LucideIcon;
+  }
+
+  const {
+    class: className,
+    size = "md",
+    color = "primary",
+    label,
+    onclick,
+    loading = false,
+    disabled = false,
+    icon: Icon,
+    ...props
+  }: Props = $props();
+</script>
+
+<BaseButton
+  class={[
+    {
+      "text-sm p-1": size === "sm",
+      "p-1.5": size === "md",
+      "text-lg p-2": size === "lg",
+    },
+    {
+      "text-primary dark:text-primary-light": color === "primary",
+      "text-neutral-950 dark:text-neutral-300": color === "secondary",
+      "text-red-600 dark:text-red-500": color === "danger",
+    },
+    {
+      "active:scale-95 hover:bg-current/10 dark:hover:bg-current/20": !disabled && !loading,
+      "opacity-70": disabled,
+    },
+    "rounded-md transition-all",
+    className,
+  ]}
+  aria-label={label}
+  {onclick}
+  {loading}
+  {disabled}
+  {...props}
+>
+  {#if loading}
+    <LoaderCircle class="animate-spin" />
+  {:else}
+    <Icon />
+  {/if}
+</BaseButton>
