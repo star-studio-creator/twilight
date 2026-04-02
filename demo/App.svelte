19dbf9cfaae2729d5295ae3e235639428295da84
FHU-yezi <yehaowei20060411@qq.com>
2026-04-03 02:31:51 +0800

    feat: 优化演示站页面宽度

--- demo/App.svelte
+++ demo/App.svelte
@@ -16,9 +16,7 @@
 
 <Toaster />
 
-<div
-  class="relative flex flex-col gap-4 text-neutral-950 dark:text-neutral-50 max-w-[60vw] mx-auto my-8"
->
+<div class="flex flex-col gap-4 w-[90vw] max-w-5xl mx-auto my-8">
   <div class="flex justify-between items-center">
     {#if isActive("/")}
       <h1 class="text-center text-2xl font-bold">暮光 / Twilight</h1>
