63606aca6795640df5c12994d1a26116d3a131ce
FHU-yezi <yehaowei20060411@qq.com>
2026-04-03 00:26:45 +0800

    perf: 为演示页面添加懒加载

--- demo/router.ts
+++ demo/router.ts
@@ -1,38 +1,22 @@
-import Alert from "@demo/pages/components/Alert.svelte";
-import Badge from "@demo/pages/components/Badge.svelte";
-import Button from "@demo/pages/components/Button.svelte";
-import Card from "@demo/pages/components/Card.svelte";
-import Checkbox from "@demo/pages/components/Checkbox.svelte";
-import Disclosure from "@demo/pages/components/Disclosure.svelte";
-import Field from "@demo/pages/components/Field.svelte";
-import Input from "@demo/pages/components/Input.svelte";
-import Modal from "@demo/pages/components/Modal.svelte";
-import Placeholder from "@demo/pages/components/Placeholder.svelte";
-import Select from "@demo/pages/components/Select.svelte";
-import Slider from "@demo/pages/components/Slider.svelte";
-import Switch from "@demo/pages/components/Switch.svelte";
-import Tabs from "@demo/pages/components/Tabs.svelte";
-import Toast from "@demo/pages/components/Toast.svelte";
-import Index from "@demo/pages/Index.svelte";
 import { createRouter } from "sv-router";
 
-export const { p, navigate, isActive, route } = createRouter({
-  "/": Index,
+export const { p, navigate, isActive, preload, route } = createRouter({
+  "/": () => import("@demo/pages/Index.svelte"),
   "/components": {
-    "/alert": Alert,
-    "/badge": Badge,
-    "/button": Button,
-    "/card": Card,
-    "/checkbox": Checkbox,
-    "/disclosure": Disclosure,
-    "/field": Field,
-    "/input": Input,
-    "/modal": Modal,
-    "/placeholder": Placeholder,
-    "/select": Select,
-    "/slider": Slider,
-    "/switch": Switch,
-    "/tabs": Tabs,
-    "/toast": Toast,
+    "/alert": () => import("@demo/pages/components/Alert.svelte"),
+    "/badge": () => import("@demo/pages/components/Badge.svelte"),
+    "/button": () => import("@demo/pages/components/Button.svelte"),
+    "/card": () => import("@demo/pages/components/Card.svelte"),
+    "/checkbox": () => import("@demo/pages/components/Checkbox.svelte"),
+    "/disclosure": () => import("@demo/pages/components/Disclosure.svelte"),
+    "/field": () => import("@demo/pages/components/Field.svelte"),
+    "/input": () => import("@demo/pages/components/Input.svelte"),
+    "/modal": () => import("@demo/pages/components/Modal.svelte"),
+    "/placeholder": () => import("@demo/pages/components/Placeholder.svelte"),
+    "/select": () => import("@demo/pages/components/Select.svelte"),
+    "/slider": () => import("@demo/pages/components/Slider.svelte"),
+    "/switch": () => import("@demo/pages/components/Switch.svelte"),
+    "/tabs": () => import("@demo/pages/components/Tabs.svelte"),
+    "/toast": () => import("@demo/pages/components/Toast.svelte"),
   },
 });
