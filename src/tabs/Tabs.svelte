f49d1f1c366a23a3a10fc941a9021a39dd47ac4f
FHU-yezi <yehaowei20060411@qq.com>
2026-04-17 16:13:01 +0800

    feat: 优化 Tabs 样式

--- src/tabs/Tabs.svelte
+++ src/tabs/Tabs.svelte
@@ -37,11 +37,11 @@
       for={itemId}
       class={[
         {
-          "text-primary dark:text-primary-light border-current shadow-current/20 peer-focus-visible:outline": item.value === active,
-          "text-secondary border-secondary-light shadow-secondary-light/20 dark:text-secondary-light dark:border-secondary-dark dark:shadow-secondary-dark/20": item.value !== active,
+          "text-primary dark:text-primary-light border-current peer-focus-visible:outline": item.value === active,
+          "text-secondary dark:text-secondary-light border-secondary-light dark:border-secondary": item.value !== active,
         },
         {
-          "cursor-pointer active:scale-95 hover:bg-current/5 dark:hover:bg-current/10 hover:shadow": !item.disabled,
+          "cursor-pointer active:scale-95 hover:bg-current/5 dark:hover:bg-current/10": !item.disabled,
           "cursor-not-allowed opacity-70": item.disabled,
         },
         "flex gap-1.5 items-center border-b-2 rounded-t px-3 py-1.5 transition-all",
