94d79391b10d105c5d24c90ebdf3ae6c34e73780
FHU-yezi <yehaowei20060411@qq.com>
2026-04-18 15:30:52 +0800

    fix: 修复 CI 报错

--- demo/pages/components/Placeholder.svelte
+++ demo/pages/components/Placeholder.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="flex flex-col gap-4 max-w-lg">
-    {#each COLORS as color}
-      <Placeholder class="w-full h-32" {color} />
-    {/each}
+  {#each COLORS as color}
+    <Placeholder class="w-full h-32" {color} />
+  {/each}
 </div>
