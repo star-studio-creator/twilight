12a388f88e7f42ebb63768d29eee3d9adaa2f700
FHU-yezi <yehaowei20060411@qq.com>
2026-05-24 03:38:30 +0800

    feat: 优化 TextButton ring 样式

--- src/button/TextButton.svelte
+++ src/button/TextButton.svelte
@@ -19,11 +19,11 @@
   }
 
   const colorVariants = {
-    primary: "text-primary-text ring-primary-border",
-    secondary: "text-secondary-text ring-secondary-border",
-    success: "text-success-text ring-success-border",
-    warning: "text-warning-text ring-warning-border",
-    danger: "text-danger-text ring-danger-border",
+    primary: "text-primary-text",
+    secondary: "text-secondary-text",
+    success: "text-success-text",
+    warning: "text-warning-text",
+    danger: "text-danger-text",
     unstyled: "",
   } satisfies Record<ColorType, ClassValue>;
 
@@ -53,7 +53,7 @@
       "active:scale-95 focus:outline-none focus-visible:ring-2": !disabled && !loading,
       "opacity-70": disabled,
     },
-    "flex items-center transition-all",
+    "flex items-center rounded-md transition-all",
     className,
   ]}
   {onclick}
