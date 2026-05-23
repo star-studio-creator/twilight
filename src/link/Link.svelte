0083451241775bc1da8997da1bd23cd37edbf14b
FHU-yezi <yehaowei20060411@qq.com>
2026-05-24 03:28:02 +0800

    feat: Link & Switch 组件聚焦时添加 ring

--- src/link/Link.svelte
+++ src/link/Link.svelte
@@ -19,7 +19,7 @@
 <BaseLink
   class={[
     "transition-colors hover:underline decoration-2 underline-offset-4",
-    "text-primary-text hover:text-primary-hover active:text-primary-active",
+    "text-primary-text hover:text-primary-hover active:text-primary-active focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-border",
     className,
   ]}
   {...props}
